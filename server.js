b154a2cba1d1a779c5c698583162f4d701a23cec
nkleinmann <nekleinmann@gmail.com>
2020-10-20 21:35:10 -0400

    corrected spelling error

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODV_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
